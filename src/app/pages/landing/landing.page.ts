ff223a98936275343c93e1509539bd6845dc1259
Jorge Vergara <javebratt@gmail.com>
2019-05-21 08:14:28 -0500

    Adds the login page

--- src/app/pages/landing/landing.page.ts
+++ src/app/pages/landing/landing.page.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.page.html',
-  styleUrls: ['./landing.page.scss'],
+  styleUrls: ['./landing.page.scss']
 })
 export class LandingPage implements OnInit {
+  constructor(
+    public loadingCtrl: LoadingController,
+    public authService: AuthService,
+    private router: Router
+  ) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async goToBillList(): Promise<void> {
+    const loading = await this.loadingCtrl.create();
+    try {
+      loading.present();
+
+      this.authService.anonymousLogin().then(() => {
+        loading.dismiss().then(() => {
+          this.router.navigateByUrl('/home');
+        });
+      });
+    } catch (error) {
+      loading.dismiss().then(() => {
+        console.error(error);
+      });
+    }
   }
-
 }
